62ee36b8763e6de76defb7ecdffc5b3e091a67a2
Leonardo de Souza <leonardo.lpds@gmail.com>
2015-05-22 02:13:00 -0300

    Criando metodo findByCategoria em PromocaoFacade

--- src/java/encontreoferta/api/facade/PromocaoFacade.java
+++ src/java/encontreoferta/api/facade/PromocaoFacade.java
@@ -1,6 +1,7 @@
 package encontreoferta.api.facade;
 
 import encontreoferta.api.model.Promocao;
+import java.util.List;
 import javax.persistence.EntityManager;
 
 public abstract class PromocaoFacade extends AbstractFacade<Promocao>{
@@ -8,7 +9,13 @@ public abstract class PromocaoFacade extends AbstractFacade<Promocao>{
     public PromocaoFacade(Class<Promocao> entityClass) {
         super(entityClass);
     }
-
+    
+    public List<Promocao> findByCategoria(Integer idCategoria){
+        return getEntityManager().createNamedQuery("Promocao.findByCategoria")
+                .setParameter("idCategoria", idCategoria)
+                .getResultList();
+    }
+    
     @Override
     protected abstract EntityManager getEntityManager();
     
