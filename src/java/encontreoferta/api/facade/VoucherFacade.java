403decc47bc1a399d0255ed2635e12902b375e90
Leonardo de Souza <leonardo.lpds@gmail.com>
2015-05-22 20:12:58 -0300

    Implementando model Visitante e Gson em VoucherFacade

--- src/java/encontreoferta/api/facade/VoucherFacade.java
+++ src/java/encontreoferta/api/facade/VoucherFacade.java
@@ -4,7 +4,9 @@ import encontreoferta.api.model.Voucher;
 import java.util.Date;
 import javax.persistence.EntityManager;
 import com.google.gson.Gson;
-import java.util.ArrayList;
+import encontreoferta.api.model.Promocao;
+import encontreoferta.api.model.Usuario;
+import encontreoferta.api.model.Visitante;
 
 public abstract class VoucherFacade extends AbstractFacade<Voucher>{
 
@@ -14,8 +16,23 @@ public abstract class VoucherFacade extends AbstractFacade<Voucher>{
     
     public Voucher gerar(String json) {
         String codigo = String.valueOf(new Date().getTime());
-        Voucher v = new Voucher(1, codigo);
-        return v;
+        
+        Visitante visitante = new Gson().fromJson(json, Visitante.class);
+        
+        Usuario usuario = new Usuario();
+        usuario.setEmail(visitante.getEmail());
+        //Persiste o objeto//
+        
+        //Trocar para find
+        Promocao promocao = new Promocao(visitante.getIdPromocao());
+        
+        Voucher voucher = new Voucher();
+        voucher.setCodigo(codigo);
+        voucher.setPromocao(promocao);
+        voucher.setUsuario(usuario);
+        //super.create(voucher);
+        
+        return voucher;
     }
     
     @Override
