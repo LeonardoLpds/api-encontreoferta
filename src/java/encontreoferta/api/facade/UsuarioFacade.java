1ef6722a3e340e105f3222bb9d81b8b678ee8f5e
Leonardo de Souza <leonardo.lpds@gmail.com>
2015-05-25 23:39:30 -0300

    Criando classe UsuarioFacade

---
+++ src/java/encontreoferta/api/facade/UsuarioFacade.java
@@ -0,0 +1,51 @@
+package encontreoferta.api.facade;
+
+import encontreoferta.api.model.Usuario;
+
+public abstract class UsuarioFacade extends AbstractFacade<Usuario>{
+
+    public UsuarioFacade(Class<Usuario> entityClass) {
+        super(entityClass);
+    }
+    
+    
+    public String register(Usuario usuario) {
+        if(findByEmail(usuario.getEmail()) != null){
+            if(findBySenha(usuario.getSenha()) != null){
+                return "{\"result\":\"erro\",\"cause\":\"Usuário já cadastrado\",\"message\":\"O email digitado já existe em nossa base de dados, tente fazer login!\"}";
+            }
+            edit(usuario);
+            return "{\"result\":\"pre\",\"cause\":\"Pré cadastro encontrado\",\"message\":\"Obrigado por finalizar seu cadastro! Todos os seus vouchers podem ser encontrados em 'meus vouchers' em sua área de usuário\"}";
+        }
+        create(usuario);
+        return "{\"result\":\"registered\",\"cause\":\"Usuário cadastrado com sucesso\",\"message\":\"Bem vindo! Aqui você encontra uma oferta e perde a preocupação!\"}";
+    }
+    
+    private Usuario findByEmail(String email){
+        Usuario usuario = null;
+        try{
+            usuario = (Usuario) getEntityManager()
+                    .createNamedQuery("Usuario.findByEmail")
+                    .setParameter("email", email)
+                    .getSingleResult();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return usuario;
+    }
+    
+    private Usuario findBySenha(String senha){
+        Usuario usuario = null;
+        try{
+            usuario = (Usuario) getEntityManager()
+                    .createNamedQuery("Usuario.findBySenha")
+                    .setParameter("email", senha)
+                    .getSingleResult();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return usuario;
+    }
+    
+    
+}
