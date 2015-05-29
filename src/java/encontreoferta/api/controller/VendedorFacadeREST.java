5c3b1654a12e9a4a6b12212a0a572909078c4133
Leonardo de Souza <leonardo.lpds@gmail.com>
2015-05-29 10:32:05 -0300

    Alterando ID do vendedor para Integer

--- src/java/encontreoferta/api/controller/VendedorFacadeREST.java
+++ src/java/encontreoferta/api/controller/VendedorFacadeREST.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package encontreoferta.api.controller;
 
 import encontreoferta.api.model.Vendedor;
@@ -20,10 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import encontreoferta.api.facade.AbstractFacade;
-/**
- *
- * @author leonardo
- */
+
 @Stateless
 @Path("vendedor")
 public class VendedorFacadeREST extends AbstractFacade<Vendedor> {
@@ -44,20 +35,20 @@ public class VendedorFacadeREST extends AbstractFacade<Vendedor> {
     @PUT
     @Path("{id}")
     @Consumes("application/json")
-    public void edit(@PathParam("id") String id, Vendedor entity) {
+    public void edit(@PathParam("id") Integer id, Vendedor entity) {
         super.edit(entity);
     }
 
     @DELETE
     @Path("{id}")
-    public void remove(@PathParam("id") String id) {
+    public void remove(@PathParam("id") Integer id) {
         super.remove(super.find(id));
     }
 
     @GET
     @Path("{id}")
     @Produces("application/json")
-    public Vendedor find(@PathParam("id") String id) {
+    public Vendedor find(@PathParam("id") Integer id) {
         return super.find(id);
     }
 
