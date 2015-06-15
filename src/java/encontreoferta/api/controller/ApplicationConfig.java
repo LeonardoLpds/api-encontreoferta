f6aef01a4439f182e20151d13e817f3c5ebab9f3
Leonardo de Souza <leonardo.lpds@gmail.com>
2015-06-15 10:33:10 -0300

    Criando AdminFacadeREST

--- src/java/encontreoferta/api/controller/ApplicationConfig.java
+++ src/java/encontreoferta/api/controller/ApplicationConfig.java
@@ -14,6 +14,7 @@ public class ApplicationConfig extends Application {
     }
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(encontreoferta.api.controller.AdminFacadeREST.class);
         resources.add(encontreoferta.api.controller.CategoriaFacadeREST.class);
         resources.add(encontreoferta.api.controller.NivelFacadeREST.class);
         resources.add(encontreoferta.api.controller.PromocaoFacadeREST.class);
